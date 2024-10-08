52c5c2c8672d5753d07652752e837a049a748716
gryzun33 <yakushevao777@gmail.com>
2024-10-08 14:17:37 +0300

    feat: add finishing program

--- file-manager/index.js
+++ file-manager/index.js
@@ -3,13 +3,32 @@ import os from 'os';
 function startFileManager() {
   const args = process.argv.slice(2);
   const argUser = args.find((arg) => arg.startsWith('--username='));
+  let userName = '';
   if (argUser) {
-    const userName = argUser.split('=')[1];
+    userName = argUser.split('=')[1];
     process.stdout.write(`Welcome to the File Manager, ${userName}!${os.EOL}`);
   }
 
   process.stdin.on('data', (input) => {
-    console.log('something to do with input');
+    const cmd = input.toString().trim();
+
+    switch (cmd) {
+      case '.exit':
+        process.exit(0);
+      default:
+        process.stdout.write(`Unknown command${os.EOL}`);
+    }
+  });
+
+  process.on('exit', () => {
+    process.stdout.write(
+      `Thank you for using File Manager, ${userName}, goodbye!${os.EOL}`
+    );
+  });
+
+  process.on('SIGINT', () => {
+    console.log('');
+    process.exit(0);
   });
 }
 
