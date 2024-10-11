065b38bf2f17dd8cd67b5c14062bbf35464b1247
gryzun33 <yakushevao777@gmail.com>
2024-10-11 12:30:09 +0300

    fix: fix copyExistFile

--- src/workWithFiles/copyExistFile.js
+++ src/workWithFiles/copyExistFile.js
@@ -1,16 +1,34 @@
 import path from 'path';
-import fs from 'fs/promises';
+import fs from 'fs';
 
 async function copyExistFile(oldPath, newPath) {
-  try {
-    const fileName = path.basename(oldPath);
-    const pathToOldFile = path.resolve(process.cwd(), oldPath);
-    const pathToNewDirectory = path.resolve(process.cwd(), newPath);
-    const pathToNewFile = path.join(pathToNewDirectory, fileName);
-    await fs.copyFile(pathToOldFile, pathToNewFile);
-  } catch {
-    console.error(`Operation failed`);
-  }
+  const fileName = path.basename(oldPath);
+  const pathToOldFile = path.resolve(process.cwd(), oldPath);
+  const pathToNewDirectory = path.resolve(process.cwd(), newPath);
+  const pathToNewFile = path.join(pathToNewDirectory, fileName);
+
+  return new Promise((resolve) => {
+    const readStream = fs.createReadStream(pathToOldFile);
+    const writeStream = fs.createWriteStream(pathToNewFile);
+    readStream.pipe(writeStream);
+
+    readStream.on('end', () => {
+      resolve();
+    });
+
+    let errorHandled = false;
+
+    const handleError = () => {
+      if (!errorHandled) {
+        console.error('Operation failed 1');
+        errorHandled = true;
+        resolve();
+      }
+    };
+
+    readStream.on('error', handleError);
+    writeStream.on('error', handleError);
+  });
 }
 
 export { copyExistFile };
